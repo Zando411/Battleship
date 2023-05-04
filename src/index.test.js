880031d68a207379f3a04af332f29b30e55e23ab
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-03 22:59:14 -0700

    new tests for makeShips and createShip

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,21 @@
 import { createShip } from './ships';
+import { makeShips } from './gameboard';
 
 describe('createShip', () => {
   test('creates a ship object with the correct length', () => {
-    const ship = createShip('Submarine', 3);
+    const ship = createShip(1, 'Submarine', 3);
     expect(ship.length).toBe(3);
   });
 
   test('ship is not sunk when hits are less than length', () => {
-    const ship = createShip('Submarine', 3);
+    const ship = createShip(1, 'Submarine', 3);
     expect(ship.sunk).toBe(false);
     ship.hit();
     expect(ship.sunk).toBe(false);
   });
 
   test('ship is sunk when hits are equal to length', () => {
-    const ship = createShip('Submarine', 3);
+    const ship = createShip(1, 'Submarine', 3);
     expect(ship.sunk).toBe(false);
     ship.hit();
     expect(ship.sunk).toBe(false);
@@ -25,7 +26,7 @@ describe('createShip', () => {
   });
 
   test('ship is not sunk when hits are greater than length', () => {
-    const ship = createShip('Submarine', 3);
+    const ship = createShip(1, 'Submarine', 3);
     expect(ship.sunk).toBe(false);
     ship.hit();
     expect(ship.sunk).toBe(false);
@@ -36,9 +37,23 @@ describe('createShip', () => {
     ship.hit();
     expect(ship.sunk).toBe(true);
   });
+  test('ship name is accessible', () => {
+    const ship = createShip(1, 'Submarine', 3);
+    expect(ship.name).toBe('Submarine');
+  });
 });
 
-test('ship name is accessible', () => {
-  const ship = createShip('Submarine', 3);
-  expect(ship.name).toBe('Submarine');
+describe('makeShips', () => {
+  test('ships are made', () => {
+    const ship = makeShips();
+    expect(ship[2].name).toBe('Cruiser');
+  });
+  test('test array length', () => {
+    const ship = makeShips();
+    expect(ship.length).toBe(7);
+  });
+  test('ships are made', () => {
+    const ship = makeShips();
+    expect(ship[3].key).toBe(3);
+  });
 });
