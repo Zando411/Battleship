a683ef6d4b33382fdc8737a921e08e1a5ce53663
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-09 22:26:12 -0700

    fix webpack

---
+++ webpack.config.mjs
@@ -0,0 +1,27 @@
+import path from 'path';
+
+export default {
+  mode: 'production',
+  devServer: {
+    static: './dist',
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(new URL(import.meta.url).pathname, '..', 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
