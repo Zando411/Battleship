3e59da7d672b4207ad46b03338ecf913f06f858d
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-10 21:39:10 -0700

    working grid generation

---
+++ src/dom.js
@@ -0,0 +1,40 @@
+function populateGrids(player1, player2) {
+  populatePlayerGrid(player1.gameboard);
+  populateOpponentGrid(player2.gameboard);
+}
+
+function populatePlayerGrid(array) {
+  const grid = document.getElementById('playerGameboard');
+  array.forEach((row) => {
+    row.forEach((cell) => {
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('gridPart');
+      cellElement.dataset.contains = cell.hasShip;
+      cellElement.dataset.cellHit = cell.isHit;
+      //   cellElement.addEventListener('click', () => {
+      //      handle click event
+      //   });
+      grid.appendChild(cellElement);
+    });
+  });
+}
+function populateOpponentGrid(array) {
+  const grid2 = document.getElementById('opponentGameboard');
+  array.forEach((row) => {
+    row.forEach((cell) => {
+      const cellElement = document.createElement('div');
+      cellElement.classList.add('gridPart');
+      cellElement.dataset.contains = cell.hasShip;
+      cellElement.dataset.cellHit = cell.isHit;
+      if (cell.hasShip !== null) {
+        cellElement.style.backgroundColor = '#d9d9d9';
+      }
+      cellElement.addEventListener('click', (e) => {
+        console.log(e.target.dataset.contains);
+      });
+      grid2.appendChild(cellElement);
+    });
+  });
+}
+
+export { populateGrids };
