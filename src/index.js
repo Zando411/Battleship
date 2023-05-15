6d1eb82af0450719d1f3b56126163bb20bce305b
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-14 22:46:37 -0700

    remove old test code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
-import { createComputer, createPlayer } from './player.js';
-import { placeShip, placeShipsRandom } from './gameboard.js';
-import {
-  addGameboardEventListeners,
-  displayShips,
-  populateGrids,
-} from './dom.js';
+import { appLoad } from './gameLoop.js';
 
-const player = createPlayer();
-const opponent = createComputer();
-placeShipsRandom(opponent);
-placeShip(player.gameboard, player.ships[1], 3, 3);
-populateGrids(player, opponent);
-displayShips(player.ships);
-
-// document.addEventListener('keydown', () => {
-//   console.log('success');
-// });
+appLoad();
