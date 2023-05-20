bd12f3b115b396ff52c43ce240dcc15b4c10b3ca
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-19 22:43:29 -0700

    remove console logs

--- src/ships.js
+++ src/ships.js
@@ -14,10 +14,8 @@ function createShip(key, name, length) {
     sinkCheck: function () {
       if (this.hits >= this.length) {
         this.sunk = true;
-        console.log('The ship is sunk!');
       } else {
         this.sunk = false;
-        console.log('You hit a ship!');
       }
     },
   };
