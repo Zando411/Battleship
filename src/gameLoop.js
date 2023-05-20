fbe111179555e293a98f005adcaec7168f33545f
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-05-19 23:08:47 -0700

    remove pseudocode

--- src/gameLoop.js
+++ src/gameLoop.js
@@ -1,13 +1,3 @@
-// create basic game loop
-// game start = place ships (for player and computer)
-// while gameOver = false:
-// player goes first, run receive attack on opponentBoard (computer)
-// update opponent board
-// computer takes turn, runs receive attack on player board with random placement.
-// if playerShips || computerShips are all sunk gameOver = true and return winning player (player with at least one alive ship)
-// break or continue looping
-// if gameOver = true return winning player name and say winner
-
 import { setElementDisplay } from './dom.js';
 import { opponent, player } from './index.js';
 
